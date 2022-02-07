5265bf7f6c882f63399b85ff67f327573c9389e9
erhic <eric.gichovi@student.moringaschool.com>
2022-02-07 15:35:54 +0300

    added function to check whether the email and the user entered in the form fields already exists, and raise and error message if the condition is true

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,19 +3,35 @@
 from enum import unique
 from re import L
 from flask_wtf import FlaskForm #the form from flask
-from wtforms import StringField,BooleanField,PasswordField,SubmitField #the field class that we are going to use/import from the flask package wtform module module
+from wtforms import StringField,BooleanField,PasswordField,SubmitField,ValidationError#the field class that we are going to use/import from the flask package wtform module module
 from wtforms.validators import DataRequired,Email,EqualTo,Length,Regexp
+from models import User
 
 class RegisterForm():
     '''
     class to create forms for registration and its input   
     '''
+    
+    # fields that are to be displayed in the register form
+    
     username= StringField('Username',validators=[DataRequired(),Length(min=3,max=30,message='Provide a valid name'),Regexp('A-Za-z',message='Your name must contain letters only')])
     email=StringField(' Email',validators=[DataRequired(),Email(),Length(min=5,max=30)])
     password=PasswordField('Password',validators=[DataRequired(),EqualTo('confirmpassword',message='Password required to match'),Length(min=5,max=64)])
     confirmpassword=PasswordField('Confirm Password',validators=[DataRequired()])
     submit=SubmitField('Submit')
     
+# a function to check if a email exist to prompt user to enter another one
+ 
+    def check_if_email_exist():
+        if User.query.filter_by(email=email.data).first():
+            raise ValidationError("Email already registered!")
+
+# a function to check if a username exist to prompt user to enter another one
+    def check_if_usrname_exist():
+        if User.query.filter_by(username=username.data).first():
+            raise ValidationError('Username already exists')
+
+         
     
     
     
