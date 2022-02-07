9793211ce5f7e84a15acc8583d5096c90db1964c
erhic <eric.gichovi@student.moringaschool.com>
2022-02-07 13:50:00 +0300

    created a forms that is to be rendered to receive the input from the user,and validate to be aplied on the/classes

---
+++ app/auth/forms.py
@@ -0,0 +1,22 @@
+# importing  the flask module that are used while creating a form
+
+from flask_wtf import FlaskForm #the form from flask
+from wtforms import StringField,BooleanField,PasswordField,SubmitField #the field class that we are going to use/import from the flask package wtform module module
+
+
+class RegisterForm():
+    '''
+    class to create forms for registration and its input   
+    '''
+    
+    
+    
+    
+    
+    
+    
+    
+class LoginForm():
+    '''
+    class to create forms for login and its input   
+    '''
