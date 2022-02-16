fa174c35621dc9d50a8b6a3012cbb1bea0f9fb47
erhic <eric.gichovi@student.moringaschool.com>
2022-02-17 01:32:22 +0300

    setting my config

--- app/config.py
+++ app/config.py
@@ -1,10 +1,61 @@
+# import os
+# os.urandom(24)
+
+# class Config:
+#     SECRET_KEY = 'thisisakey'
+#     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:1234@localhost/pitchblog'
+
+
+#     UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+#     #  email configurations
+#     MAIL_SERVER = 'smtp.googlemail.com'
+#     MAIL_PORT = 587
+#     MAIL_USE_TLS = True
+#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+#     SUBJECT_PREFIX = 'Pitch'
+#     SENDER_EMAIL = 'erihngug@gmail.com'
+
+#     @staticmethod
+#     def init_app(app):
+#         pass
+
+
+# class ProdConfig(Config):
+#     '''
+#     Production  configuration child class
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+#     # SQLALCHEMY_DATABASE_URI =os.environ.get('SQLALCHEMY_DATABASE_URI')
+#     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:1234@localhost/pitches'
+
+# # class TestConfig(Config):
+# #     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:1234@localhost/pitches_test'
+
+
+# class DevConfig(Config):
+#     '''
+#     Development  configuration child class
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+#     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:1234@localhost/pitches'
+
+#     DEBUG = True
+
+# config_options = {
+# 'development':DevConfig,
+# 'production':ProdConfig,
+# 'test':TestConfig
+# }
 import os
 os.urandom(24)
 
 class Config:
     SECRET_KEY = 'thisisakey'
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:1234@localhost/pitchblog'
-
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
 
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
@@ -14,8 +65,9 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SUBJECT_PREFIX = 'Pitch'
-    SENDER_EMAIL = 'erihngug@gmail.com'
+    # SUBJECT_PREFIX = 'Post'
+    # SENDER_EMAIL = 'hing@gmail.com'
+    print(MAIL_USERNAME)
 
     @staticmethod
     def init_app(app):
@@ -27,12 +79,15 @@ class ProdConfig(Config):
     Production  configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
-    '''
-    # SQLALCHEMY_DATABASE_URI =os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:1234@localhost/pitches'
-
+    # '''
+   
+    SQLALCHEMY_DATABASE_URI =os.environ.get('SQLALCHEMY_DATABASE_URI')
+    
+    
+    
+  
 # class TestConfig(Config):
-#     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:1234@localhost/pitches_test'
+#     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:1234@localhost/blog_test'
 
 
 class DevConfig(Config):
@@ -41,12 +96,5 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:1234@localhost/pitches'
 
     DEBUG = True
-
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig,
-# 'test':TestConfig
-# }
